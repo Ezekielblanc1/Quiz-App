36c41c3b80fa0155511f31a0c85b7a63a559c6bb
Ezekiel blanc <ezekielblanc1@gmail.com>
2019-02-28 18:03:54 +0100

    retrieved scores from local storage

--- end.js
+++ end.js
@@ -2,7 +2,11 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const recentScore = localStorage.getItem('recentScore')
-console.log(recentScore)
+// console.log(recentScore)
+
+const MAX_HIGH_SCORES = 5;
+const highScores = JSON.parse(localStorage.getItem('highScores') || []);
+// console.log(highScores)
 
 finalScore.textContent = recentScore
 
@@ -13,5 +17,14 @@ username.addEventListener('keyup', () => {
 
 const saveHighScore = e => {
   e.preventDefault();
-  console.log('Clicked')
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value
+  }
+  highScores.push(score);
+  highScores.sort((a,b) => b.score - a.score);
+  highScores.splice(5);
+
+  localStorage.setItem('highScores', JSON.stringify(highScores))
+  window.location.assign('/')
 }
